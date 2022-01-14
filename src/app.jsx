00cb53a7fe6d230ec089feafedbcd6a14ccab742
Jon Kang <jk2060@georgetown.edu>
2022-01-13 21:54:20 -0500

    progress commit - add react router dom and firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,15 @@
 import "./app.css";
 import Login from "./components/login/login";
+import Landing from "./components/landing/landing";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className='app'>
-      <Login />;
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='app' element={<Landing />} />
+      </Routes>
     </div>
   );
 };
